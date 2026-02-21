83299590b55274a37c1f7b616331c9ca25817eff
dalvarez-umg <dalvareze@miumg.edu.gt>
2026-02-21 11:45:59 -0600

    Semana4
    ejemplos

--- Semana III/PrimerPuntero.cpp
+++ Semana III/PrimerPuntero.cpp
@@ -2,17 +2,7 @@
 using namespace std;
 
 int main() {
-    /*
-     * Se reserva memoria en el stack
-     * x contiene 5
-     */
     int x = 5;        // Variable normal
-    /*
-     * &x obtiene la dirección de memoria de x
-     * p almacena esa dirección
-     * p NO almacena 5
-     * p almacena la dirección de memoria 0xec75dff984
-     */
     int* p = &x;      // Puntero que guarda la dirección de x
 
     cout << "Valor de x: " << x << endl;
@@ -20,11 +10,5 @@ int main() {
 
     cout << "Valor de p (direccion que almacena): " << p << endl;
     cout << "Valor al que apunta p (*p): " << *p << endl;
-
-    x = 20;
-    cout << "---- DESPUES DE *p = 20 ----" << endl;
-    cout << "Valor de x: " << x << endl;
-    cout << "Valor de *p: " << *p << endl;
-
     return 0;
 }
---
+++ Test/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int* p;
+    cout << "Valor de p direccion " << &p << endl;
+    return 0;
+}
